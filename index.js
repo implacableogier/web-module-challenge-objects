8f060e6bc85734b2e27912de206c04b92bb7694f
Jason <jason-cummings@lambdastudents.com>
2020-09-27 18:00:13 -0700

    began task 6

--- index.js
+++ index.js
@@ -91,8 +91,8 @@ and should return a string in the format `{name} gave the restaurant a {rating},
  * For example, if getReviewByIndex is invoked with reviews and the number 0
  * it will return `Daniela gave the restaurant a 5 star review and their feedback was: Beautiful atmosphere and wonderful vegan options!`
 */
-function getReviewByIndex(reviews, index) {
-    /* code here */
+function getReviewByIndex(array, index) {
+    let 
   }
   
 
